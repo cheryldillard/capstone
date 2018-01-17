1e7ac8bc539a31380aa93277ca3207ba3de77411
Cheryl Dillard <cheryldillard4@gmail.com>
2018-01-17 02:16:57 -0600

    change html elements

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      // message: "Welcome to your Personal Prescription Manager!",
+      message: "",
       prescriptions: [],
       currentPrescription: {},
       numberFilter: "",
